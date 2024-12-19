1b85a93df1cae3f8690fca76ef2ff3177a6944cb
javlonumrzokov <jackumrzokov@gmail.com>
2024-12-19 22:49:11 +0500

    feat: develop likeTargetMember GraphQL API

--- apps/nestar-api/src/components/member/member.resolver.ts
+++ apps/nestar-api/src/components/member/member.resolver.ts
@@ -72,6 +72,17 @@ export class MemberResolver {
 		return await this.memberService.getAgents(memberId, input);
 	}
 
+	@UseGuards(AuthGuard)
+	@Mutation(() => Member)
+	public async likeTartgetMember(
+		@Args('memberId') input: string,
+		@AuthMember('_id') memberId: ObjectId,
+	): Promise<Member> {
+		console.log('Mutation: likeTartgetMember');
+		const likeRefId = shapeIntoMongoObjectId(input);
+		return await this.memberService.likeTartgetMember(memberId, likeRefId);
+	}
+
 	/** ADMIN */
 
 	@Roles(MemberType.ADMIN)
